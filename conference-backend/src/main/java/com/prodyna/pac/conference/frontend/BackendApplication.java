709c64b97c680916c53ac3d7125ae88b244813f6
Darko Krizic <darko@krizic.net>
2019-01-16 12:39:02 +0100

    Java 11 and Spring Boot 2.1.2

--- conference-backend/src/main/java/com/prodyna/pac/conference/frontend/BackendApplication.java
+++ conference-backend/src/main/java/com/prodyna/pac/conference/frontend/BackendApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 // @Import(SpringDataRestConfiguration.class) - does currently not work with Spring 2.0, when SpringFox 3.0 is available, try again
-public class Application {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 }
--- conference-frontend/src/main/java/com/prodyna/pac/conference/frontend/FrontendApplication.java
+++ conference-frontend/src/main/java/com/prodyna/pac/conference/frontend/FrontendApplication.java
@@ -2,13 +2,12 @@ package com.prodyna.pac.conference.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 // @Import(SpringDataRestConfiguration.class) - does currently not work with Spring 2.0, when SpringFox 3.0 is available, try again
-public class Application {
+public class FrontendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(FrontendApplication.class, args);
 	}
 }
